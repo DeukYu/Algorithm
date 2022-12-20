379fb33eda28acff58b9e5086d43d7e40b2ed232
DeukYu <ldy8070@naver.com>
2022-12-20 23:29:02 +0900

    Create 2979_트럭_주차.cpp

---
+++ Algorithm/Algorithm/2979_트럭_주차.cpp
@@ -0,0 +1,15 @@
+// BOJ 2979번 트럭 주차
+#include <iostream>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+void soultion(string str)
+{
+}
+
+int main()
+{
+	vector<int, vector<int>> values;
+}
