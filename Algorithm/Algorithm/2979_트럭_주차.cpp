53963c0d0bed21f32e628c53f91a3951db76545b
DeukYu <ldy8070@naver.com>
2022-12-30 17:13:26 +0900

    Update 2979_트럭_주차.cpp

--- Algorithm/Algorithm/2979_트럭_주차.cpp
+++ Algorithm/Algorithm/2979_트럭_주차.cpp
@@ -2,36 +2,35 @@
 #include <iostream>
 #include <algorithm>
 #include <vector>
+#include <array>
 
 using namespace std;
 
-void soultion(string str)
+void soultion(int carNum)
 {
+	array<int, 100> cnt{};
+	int A, B, C;
+	int result = 0;
+	cin >> A >> B >> C;
+	for (int i = 0; i < carNum; ++i)
+	{
+		int arrival, leave;
+		cin >> arrival >> leave;
+		for_each(cnt.begin() + arrival, cnt.begin() + leave, [](auto& num) {++num; });
+	}
+	for (const int& num : cnt)
+	{
+		if (num > 0)
+		{
+			if (num == 1) result += A;
+			else if (num == 2) result += B * 2;
+			else if (num == 3) result += C * 3;
+		}
+	}
+	cout << result << endl;
 }
 
 int main()
 {
-	int cnt[100]{};	// 카운트를 위한 배열
-	int result = 0;	// 결과값
-	int A, B, C;	// 각 주차 대당 값
-	cin >> A >> B >> C;
-	for (int i = 0; i < 3; ++i)
-	{
-		int a, b;
-		cin >> a >> b;
-		for (int j = a; j < b; ++j)
-			cnt[j] += 1;
-	}
-	for (int i = 0; i < 100; ++i)
-	{
-		if (cnt[i] > 0)
-		{
-			if (cnt[i] == 1) result += A;
-			else if (cnt[i] == 2) result += B * 2;
-			else if (cnt[i] == 3) result += C * 3;
-		}
-	}
-
-	cout << result << endl;
-	return 0;
+	soultion(3);
 }
