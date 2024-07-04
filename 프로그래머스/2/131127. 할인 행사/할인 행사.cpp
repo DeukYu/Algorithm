819cb57634bc2b58b666b25e25616e4c7d0bfd47
Deuk Yu Lee <ldy8070@gmail.com>
2024-07-04 11:50:16 +0900

    [level 2] Title: 할인 행사, Time: 0.02 ms, Memory: 3.59 MB -BaekjoonHub

--- 프로그래머스/2/131127. 할인 행사/할인 행사.cpp
+++ 프로그래머스/2/131127. 할인 행사/할인 행사.cpp
@@ -5,6 +5,7 @@ using namespace std;
 
 int solution(vector<string> want, vector<int> number, vector<string> discount) {
 	int answer = 0;
+	int discountPeriod = 10;
 
 	map<string, int> wantMap, tempMap;
 	for (int i = 0; i < want.size(); ++i)
@@ -12,24 +13,18 @@ int solution(vector<string> want, vector<int> number, vector<string> discount) {
 		wantMap[want[i]] = number[i];
 	}
 
-	for(int i = 0; i + 10 <= discount.size(); ++i)
+	for(int i = 0; i <= discount.size() - discountPeriod; ++i)
 	{
 		tempMap = wantMap;
-		int t = i + 10;
 		map<string, int> discountMap;
-		for (int j = i; j < t; ++j)
+		for (int j = i; j < i + discountPeriod; ++j)
 		{
-			discountMap[discount[j]]++;
-		}
-
-		for (auto iter : discountMap)
-		{
-			if (tempMap.find(iter.first) != tempMap.end())
+			if (tempMap.find(discount[j]) != tempMap.end())
 			{
-				int num = tempMap[iter.first] - iter.second;
-				if (num <= 0)
+				tempMap[discount[j]]--;
+				if (tempMap[discount[j]] <= 0)
 				{
-					tempMap.erase(iter.first);
+					tempMap.erase(discount[j]);
 				}
 			}
 		}
