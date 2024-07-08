89b88c0ac46ac6c26a6ea8920ce58f9a9d48de3f
Deuk Yu Lee <ldy8070@gmail.com>
2024-07-08 14:46:15 +0900

    [level 2] Title: 택배상자, Time: 9.15 ms, Memory: 38.5 MB -BaekjoonHub

--- 프로그래머스/2/131704. 택배상자/택배상자.cpp
+++ 프로그래머스/2/131704. 택배상자/택배상자.cpp
@@ -4,28 +4,28 @@
 using namespace std;
 
 int solution(vector<int> order) {
-    int answer = 0;
+	int answer = 0;
 
-    stack<int> s;
+	stack<int> s;
 
 	int i = 0;
 
-	for(int box = 1 ; box <= order.size() ; box++)
+	for (int box = 1; box <= order.size(); ++box)
 	{
-		if(order[i] != box)
+		if (order[i] != box)
 		{
 			s.push(box);
 			continue;
 		}
 		++i;
 		++answer;
-		while(!s.empty() && s.top() == order[i])
+		while (!s.empty() && s.top() == order[i])
 		{
 			s.pop();
-			answer++;
-			i++;
+			++answer;
+			++i;
+
 		}
 	}
-
 	return answer;
 }
